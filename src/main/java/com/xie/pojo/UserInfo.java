8037f4e8ae9c38f830c6ab2849d316f707509e00
xwh1108 <35356435+xwh1108@users.noreply.github.com>
2019-05-05 21:09:10 +0800

    加入Security相关实体类及依赖

---
+++ src/main/java/com/xie/pojo/UserInfo.java
@@ -0,0 +1,17 @@
+package com.xie.pojo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserInfo {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String phoneNum;
+    private int status;
+    private String statusStr;
+    private List<Role> roles;
+}
--- src/main/java/com/xie/web/IndexController.java
+++ src/main/java/com/xie/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.xie.web;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
